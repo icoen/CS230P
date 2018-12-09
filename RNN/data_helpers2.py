74be8273b604293c6dd0ac72bd335c9b37eb6ac5
cuizy3 <cuizy3@gmail.com>
2018-12-09 01:11:01 -0500

    removed extra preprocess in data helpers2

--- RNN/data_helpers2.py
+++ RNN/data_helpers2.py
@@ -8,6 +8,7 @@ def clean_str(string):
     Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
     """
     string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+    """
     string = re.sub(r"\'s", " \'s", string)
     string = re.sub(r"\'ve", " \'ve", string)
     string = re.sub(r"n\'t", " n\'t", string)
@@ -20,6 +21,7 @@ def clean_str(string):
     string = re.sub(r"\)", " \) ", string)
     string = re.sub(r"\?", " \? ", string)
     string = re.sub(r"\s{2,}", " ", string)
+    """
     return string.strip().lower()
 
 
