c979871246035644580f40efc04ecb6bf7e23bf7
cuizy3 <cuizy3@gmail.com>
2018-12-09 01:32:57 -0500

    data_helpers2

--- RNN/data_helpers2.py
+++ RNN/data_helpers2.py
@@ -7,8 +7,8 @@ def clean_str(string):
     Tokenization/string cleaning for all datasets except for SST.
     Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
     """
-    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
-    """
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\`\.]", " ", string)
+    
     string = re.sub(r"\'s", " \'s", string)
     string = re.sub(r"\'ve", " \'ve", string)
     string = re.sub(r"n\'t", " n\'t", string)
@@ -21,7 +21,7 @@ def clean_str(string):
     string = re.sub(r"\)", " \) ", string)
     string = re.sub(r"\?", " \? ", string)
     string = re.sub(r"\s{2,}", " ", string)
-    """
+    string=' '.join(word for word in sumstring.split())
     return string.strip().lower()
 
 
